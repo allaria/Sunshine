7764467a92d581d1f7fdeddbfcfef8955a5cb230
Alberto Laria <alaria@entelibai.local>
2016-04-12 08:28:49 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/sunshine/app/Constantes.java
@@ -0,0 +1,15 @@
+package com.example.android.sunshine.app;
+
+/**
+ * Created by alaria on 05/04/2016.
+ */
+public final class Constantes {
+    final static String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
+    final static String QUERY_PARAM = "q";
+    final static String FORMAT_PARAM = "mode";
+    final static String UNITS_PARAM = "units";
+    final static String DAYS_PARAM = "cnt";
+    final static String APPID_PARAM = "appid";
+    final static String OPEN_WEATHER_MAP_API_KEY = "cf990792e81c4b950d06ff1321a0ee63";
+
+}
